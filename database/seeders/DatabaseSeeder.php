9be32edd25a4cb956aca8817b7906ffd53b66df6
CeansBzh <ebriantais@orange.fr>
2022-09-06 14:10:21 +0200

    Ajout objet réservation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->create([
+        $user = \App\Models\User::factory()->create([
             'firstname' => 'Michel',
             'lastname' => 'Dupont',
             'email' => 'michel@dupont.com',
+        ]);
+
+        \App\Models\Booking::factory()->create([
+            'user_id' => $user->id,
+            'start' => '2023-01-01',
+            'end' => '2023-01-10',
+            'nb_people' => 2,
+            'status' => 'paid_for',
         ]);
     }
 }
