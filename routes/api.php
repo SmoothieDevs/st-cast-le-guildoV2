658de2cacf64c9435f60509b72282e7c637624f3
CeansBzh <ebriantais@orange.fr>
2022-11-02 15:24:40 +0100

    Authentification pour l'api des disponibilités

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/availabilities', function () {
-    return new BookingAvailabilityCollection(BookingAvailability::getAvailableDates());
-})->name('availabilities.index');
+    Route::get('/availabilities', function () {
+        return new BookingAvailabilityCollection(BookingAvailability::getAvailableDates());
+    })->name('availabilities.index');
+});
