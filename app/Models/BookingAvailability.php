c10cf4fd0cefd8db1aed646b5b35906e7504064e
CeansBzh <ebriantais@orange.fr>
2022-12-31 14:25:15 +0100

    Correction mauvais calcul disponibilités

--- app/Models/BookingAvailability.php
+++ app/Models/BookingAvailability.php
@@ -10,55 +10,69 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BookingAvailability extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array
-   */
-  protected $fillable = ['from', 'to'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['from', 'to'];
 
-  /**
-   * The storage format of the model's date columns.
-   *
-   * @var string
-   */
-  protected $dateFormat = 'Y-m-d';
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d';
 
-  /**
-   * The attributes that should be cast.
-   *
-   * @var array<string, string>
-   */
-  protected $casts = [
-    'from' => 'date:Y-m-d',
-    'to' => 'date:Y-m-d',
-  ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'from' => 'date:Y-m-d',
+        'to' => 'date:Y-m-d',
+    ];
 
-  public static function getAvailableDates()
-  {
-    $availabilities = self::whereDate('to', '>=', now())->select('from', 'to')->get();
-    $availabilitiesCollection = $bookingsCollection = new PeriodCollection();
-    $bookings = Booking::where('status', BookingStatus::Validated)->select('start', 'end')->get();
+    public static function getAvailableDates()
+    {
+        $availabilities = self::whereDate('to', '>=', now())->select('from', 'to')->get();
+        $availabilitiesCollection = $bookingsCollection = new PeriodCollection();
+        $bookings = Booking::where('status', BookingStatus::Validated)->select('start', 'end')->get();
 
-    // Avec le package spatie/period, calcul de la différence suivante: "périodes_disponibles - périodes_réservées"
-    // Création des collections de dates
-    foreach ($availabilities as $availability) {
-      $availabilitiesCollection = $availabilitiesCollection->add(Period::make($availability['from'], $availability['to']));
+        // Avec le package spatie/period, calcul de la différence suivante: "périodes_disponibles - périodes_réservées"
+        // Création des collections de dates
+        foreach ($availabilities as $availability) {
+            $availabilitiesCollection = $availabilitiesCollection->add(Period::make($availability['from'], $availability['to']));
+        }
+        $boundaries = $availabilitiesCollection->boundaries();
+        $gaps = $availabilitiesCollection->gaps();
+        $availabilitiesCollection = new PeriodCollection();
+        $availabilitiesCollection = $availabilitiesCollection->add($boundaries);
+        $availabilitiesCollection = $availabilitiesCollection->subtract($gaps);
+        foreach ($bookings as $booking) {
+            $bookingsCollection = $bookingsCollection->add(Period::make($booking['start'], $booking['end']));
+        }
+
+        $availabilities = [];
+        foreach ($availabilitiesCollection as $availability) {
+            $availabilities[] = new BookingAvailability([
+                'from' => $availability->start()->format('Y-m-d'),
+                'to' => $availability->end()->format('Y-m-d'),
+            ]);
+        }
+
+        $availabilitiesWithBooked = [];
+        // Calcul de la différence et création d'un tableau de périodes disponibles
+        foreach ($availabilitiesCollection->subtract($bookingsCollection) as $availability) {
+            $availabilitiesWithBooked[] = [
+                'from' => $availability->start()->format('Y-m-d'),
+                'to' => $availability->end()->format('Y-m-d'),
+            ];
+        }
+
+        return ['availabilities' => $availabilities, 'bookings' => $bookings, 'availabilitiesWithBooked' => $availabilitiesWithBooked];
     }
-    foreach ($bookings as $booking) {
-      $bookingsCollection = $bookingsCollection->add(Period::make($booking['start'], $booking['end']));
-    }
-
-    $availabilitiesWithBooked = [];
-    // Calcul de la différence et création d'un tableau de périodes disponibles
-    foreach ($availabilitiesCollection->subtract($bookingsCollection) as $availability) {
-      $availabilitiesWithBooked[] = [
-        'from' => $availability->start()->format('Y-m-d'),
-        'to' => $availability->end()->format('Y-m-d'),
-      ];
-    }
-    return ['availabilities' => $availabilities, 'bookings' => $bookings, 'availabilitiesWithBooked' => $availabilitiesWithBooked];
-  }
 }
